c205f61052ee44ecc04ec3e4fa38487bd1e1352b
Paul Brannan <curlypaul924@gmail.com>
2012-06-19 10:42:17 -0400

    Fixed potential crash

--- ext/with_ext.c
+++ ext/with_ext.c
@@ -17,7 +17,9 @@ static VALUE with_s_create(VALUE klass, VALUE obj, VALUE self_obj)
   rb_iv_set(s, "__with_obj__", obj);
   rb_iv_set(s, "__with_self_obj__", self_obj);
 
-  /* Copy the instance variable table from self_obj */
+  /* Copy the pointer to the instance variable table from self_obj.  As
+   * long as we hold a reference to self_obj, this pointer should be
+   * valid. */
   struct RBasic basic = *(RBASIC(self));
   *(ROBJECT(self)) = *(ROBJECT(self_obj));
   *(RBASIC(self)) = basic;
@@ -35,6 +37,22 @@ static VALUE with_self_obj(VALUE self)
   return rb_iv_get(rb_singleton_class(self), "__with_self_obj__");
 }
 
+static VALUE with_cleanup(VALUE self)
+{
+  /* We don't want to keep the ivar table pointer around indefinitely,
+   * because if we do, the GC will free the ivar table, which is
+   * undesirable, since the original object still references it.  So we
+   * set the ivar table back to something that won't get freed, instead.
+   */
+
+  NEWOBJ(dummy, struct RObject);
+  OBJSETUP(dummy, rb_cWith, T_OBJECT);
+
+  struct RBasic basic = *(RBASIC(self));
+  *(ROBJECT(self)) = *(ROBJECT(dummy));
+  *(RBASIC(self)) = basic;
+}
+
 void Init_with_ext()
 {
   VALUE super = rb_class_boot(0);
@@ -48,5 +66,6 @@ void Init_with_ext()
   rb_define_method(rb_cWith, "__instance_eval__", rb_obj_instance_eval, -1);
   rb_define_method(rb_cWith, "__with__obj__", with_obj, 0);
   rb_define_method(rb_cWith, "__with__self_obj__", with_self_obj, 0);
+  rb_define_method(rb_cWith, "__with__cleanup__", with_cleanup, 0);
 }
 
