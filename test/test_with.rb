cf0176e9e581d5d410bb4d52052bac5963efb57b
Paul Brannan <curlypaul924@gmail.com>
2012-06-19 09:35:05 -0400

    Added test for iteration

--- test/test_with.rb
+++ test/test_with.rb
@@ -22,6 +22,16 @@ class WithTest < Test::Unit::TestCase
     end
   end
 
+  class DelegateObj2
+    def initialize
+      @ivar = "delegate_obj_2_ivar"
+    end
+
+    def delegate_obj_method
+      return "delegate_obj_2_method"
+    end
+  end
+
   def test_with_can_call_self_obj
     self_obj = SelfObj.new
     delegate_obj = DelegateObj.new
@@ -53,6 +63,19 @@ class WithTest < Test::Unit::TestCase
       end
     end
     assert_equal "self_obj_ivar", result
+  end
+
+  def test_with_can_iterate_over_delegates
+    self_obj = SelfObj.new
+    delegate_obj_1 = DelegateObj.new
+    delegate_obj_2 = DelegateObj2.new
+    results = [ ]
+    self_obj.instance_eval do
+      with(delegate_obj_1, delegate_obj_2) do
+        results << delegate_obj_method()
+      end
+    end
+    assert_equal [ "delegate_obj_method", "delegate_obj_2_method" ], results
   end
 end
 
